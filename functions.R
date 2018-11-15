114c9e974e579b3522cae0b0eb7c9b66b0646ddd
Joel Nitta <joelnitta@gmail.com>
2018-11-15 13:51:02 +0900

    Add initiate_booklist and update_booklist

--- functions.R
+++ functions.R
@@ -1,3 +1,26 @@
+#' Create a google sheet with data imported from Zotero
+#'
+#' Google drive allows for files of the same name to exist in the same folder.
+#' If the sheet_name already exists, a warning will be issued but it will still
+#' be written. So make sure you aren't using a duplicated sheet name!
+#'
+#' @param sheet_name String; name to use for the google sheet
+#' @param groupID String; ID number of Zotero group with bibliography
+#' @param api_key String; API key needed to access Zotero group
+#'
+#' @return NULL; a google sheet will be created with the bibliography from
+#' Zotero and new columns for purchase and loan status
+#' @export
+initiate_booklist <- function (sheet_name, groupID, api_key) {
+  get_zotero(groupID = groupID, api_key = api_key) %>%
+    dplyr::mutate(purchased = "",
+           date_purchased = "",
+           checked_out = "",
+           date_checked_out = "",
+           who_checked_out = "") %>%
+    googlesheets::gs_new(sheet_name, input = ., trim = TRUE)
+}
+
 #' get_zotero
 #'
 #' Download a bibliography from a Zotero group 
@@ -11,16 +34,16 @@
 #' @param api_key API key issued by the group admin
 #'
 #' @return A tibble
-
+#' @export
 get_zotero <- function (groupID, api_key) {
   RefManageR::ReadZotero(group = (groupID),
                          .params = list(
                            key = (api_key)
                          )) %>%
     as.data.frame %>%
-    as_tibble(rownames = "bookID") %>%
-    mutate(title = str_remove_all(title, "\\{|\\}")) %>%
-    select(bookID, title, author, abstract, year, publisher, edition, isbn)
+    dplyr::as_tibble() %>%
+    dplyr::mutate_all(~str_remove_all(., "\\{|\\}")) %>%
+    dplyr::select(title, author, year, publisher, edition, isbn)
 }
 
 #' load_gs_data
@@ -32,7 +55,7 @@ get_zotero <- function (groupID, api_key) {
 #' @param table Name of google docs table
 #'
 #' @return dataframe
-
+#' @export
 load_gs_data <- function(table) {
   # Grab the Google Sheet
   sheet <- googlesheets::gs_title(table)
@@ -42,32 +65,61 @@ load_gs_data <- function(table) {
     purrr::map_df(as.character)
 }
 
-#' update_gs_cells
-#' 
-#' Edit cells row-wise in a google sheet for a selected row
-#'
-#' @param new_vector Character vector of new values to enter into row, one per cell
-#' @param key_select Character with value that matches single value in key column
-#' @param key_col Name of "key" column used to identify where to edit cells
-#' @param gs_data Original data from the google sheet (obtained using load_gs_data)
-#' @param gs_object googlesheets object corresponding to gs_data
-#'
-#' @return a googlesheets object
 
-update_gs_cells <- function (new_vector, key_select, key_col = "bookID", gs_data, gs_object) {
-  row_select <- paste0("B", which(gs_data[[key_col]] == key_select) + 1)
-  gs_object %>%
-    gs_edit_cells(input = new_vector, anchor = row_select, byrow = TRUE, trim = FALSE)
+#' Verify that all ISBN numbers are unique
+#'
+#' @param data Dataframe including at least one column called "isbn"
+#' @param data_source Either "Google" or "Zotero"
+#'
+#' @return TRUE if there are no duplicate ISBN numbers or an error stating
+#' which ISBN numbers are duplicates.
+check_isbn <- function(data, data_source = c("Google", "Zotero")) {
+  dup_isbn <-
+    data %>%
+    dplyr::count(isbn) %>%
+    dplyr::filter(n > 1) %>%
+    dplyr::pull(isbn) %>%
+    paste(collapse = ", ")
+  
+  assertthat::assert_that(
+    dup_isbn == "",
+    msg = glue::glue("Duplicate ISBNs detected in {data_source} data: {dup_isbn}. 
+                     Please remove these before updating.")
+    )
 }
 
-format_books <- function (books_df, width_percent_mod, cols_select) {
-  widths <- round(str_length(books_df$abstract) * 0.01 * width_percent_mod) + 4
-  widths[is.na(widths)] <- 4
+
+#' Syncronize a Zotero bibliography and a Google Sheet
+#'
+#' @param groupID String; ID number of Zotero group with bibliography
+#' @param api_key String; API key needed to access Zotero group
+#' @param sheet_name String; name to use for the google sheet
+#'
+#' @return NULL; the google sheet should be updated with new book entries from Zotero, if any.
+#' @export
+update_booklist <- function (groupID, api_key, sheet_name) {
   
-  books_df %>%
-    mutate(
-      abstract = 
-        map2_chr(abstract, widths, str_trunc)
-    ) %>%
-    select(cols_select)
+  # Read in current bibliographic data from Zotero
+  zotero_data <- get_zotero(groupID = groupID, 
+                            api_key = api_key)
+  
+  # Make sure ISBNs are unique in Zotero data
+  check_isbn(zotero_data, "Zotero")
+  
+  # Get book status data (whether purchased or checked out) from googlesheets 
+  book_status <- load_gs_data("booklist") %>%
+    dplyr::select(isbn, contains("purchased"), contains("checked_out"))
+  
+  # Make sure ISBNs are unique in google data
+  check_isbn(book_status, "Google")
+  
+  # Merge Zotero data with book status and update google sheet
+  book_update <-
+    dplyr::full_join(zotero_data, book_status) %>%
+    dplyr::mutate_all(~ifelse(is.na(.), "", .))
+  
+  # Update googlesheet with new data
+  googlesheets::gs_title(sheet_name) %>%
+    googlesheets::gs_edit_cells(input = book_update)
+  
 }
