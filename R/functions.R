f73587c78c058c456d7ea5b32680404dcc7d8fda
Joel Nitta <joelnitta@gmail.com>
2018-11-15 17:06:45 +0900

    Fix googlesheet to googlesheets

--- R/functions.R
+++ R/functions.R
@@ -6,23 +6,23 @@
 #' @param groupID ID number of Zotero group with bibliography
 #' @param api_key API key needed to access Zotero group
 #'
-#' @return A \code{\link[googlesheet]{googlesheets}} object; a google sheet 
-#' will be created using data from Zotero, and new columns for purchase and 
+#' @return A \code{\link[googlesheets]{googlesheets}} object; a google sheet
+#' will be created using data from Zotero, and new columns for purchase and
 #' loan status will be added.
 #' @export
 initiate_booklist <- function (sheet_name, groupID, api_key) {
-  
+
   # Check if any files in drive root folder are already present with same name
   # as sheet_name
   already_in_drive <- googledrive::drive_find(
     pattern = sheet_name,
     q = "'root' in parents") %>%
     dplyr::pull(name)
-  
+
   assertthat::assert_that(
     !any(sheet_name %in% already_in_drive),
     msg = "A sheet with the same name already exists in your google drive root folder")
-  
+
   get_zotero(groupID = groupID, api_key = api_key) %>%
     dplyr::mutate(purchased = "",
            date_purchased = "",
@@ -34,18 +34,18 @@ initiate_booklist <- function (sheet_name, groupID, api_key) {
 
 #' Import data from Zotero
 #'
-#' Download a bibliography from a Zotero group 
+#' Download a bibliography from a Zotero group
 #' and convert it to a tibble (tidy dataframe).
 #'
-#' To find the groupID of your private Zotero group, look at its URL. 
-#' The groupID is the part just before the group name. For example, 
-#' the groupID of 
-#' 
+#' To find the groupID of your private Zotero group, look at its URL.
+#' The groupID is the part just before the group name. For example,
+#' the groupID of
+#'
 #' https://www.zotero.org/groups/2220437/mygroup_01/
 #'
 #' is 2220437.
 #'
-#' @param groupID Zotero groupID (not group name); 
+#' @param groupID Zotero groupID (not group name);
 #' precedes group name in group URL
 #' @param api_key API key issued by the group admin
 #'
@@ -63,7 +63,7 @@ get_zotero <- function (groupID, api_key) {
 }
 
 #' Load a google sheet as a dataframe
-#' 
+#'
 #' From https://shiny.rstudio.com/articles/persistent-data-storage.html
 #'
 #' @param table Name of google sheet
@@ -94,17 +94,17 @@ check_isbn <- function(data, data_source = c("Google", "Zotero")) {
     dplyr::filter(n > 1) %>%
     dplyr::pull(isbn) %>%
     paste(collapse = ", ")
-  
+
   assertthat::assert_that(
     dup_isbn == "",
-    msg = glue::glue("Duplicate ISBNs detected in {data_source} data: {dup_isbn}. 
+    msg = glue::glue("Duplicate ISBNs detected in {data_source} data: {dup_isbn}.
                      Please remove these before updating.")
     )
 }
 
 
 #' Synchronize a Zotero bibliography and a Google Sheet
-#' 
+#'
 #' It is assumed that the google sheet is located in the root folder of the
 #' user's Google Drive.
 #'
@@ -115,28 +115,28 @@ check_isbn <- function(data, data_source = c("Google", "Zotero")) {
 #' @return NULL; the google sheet should be updated with new book entries from Zotero, if any.
 #' @export
 update_booklist <- function (groupID, api_key, sheet_name) {
-  
+
   # Read in current bibliographic data from Zotero
-  zotero_data <- get_zotero(groupID = groupID, 
+  zotero_data <- get_zotero(groupID = groupID,
                             api_key = api_key)
-  
+
   # Make sure ISBNs are unique in Zotero data
   check_isbn(zotero_data, "Zotero")
-  
-  # Get book status data (whether purchased or checked out) from googlesheets 
+
+  # Get book status data (whether purchased or checked out) from googlesheets
   book_status <- load_gs_data("booklist") %>%
     dplyr::select(isbn, contains("purchased"), contains("checked_out"))
-  
+
   # Make sure ISBNs are unique in google data
   check_isbn(book_status, "Google")
-  
+
   # Merge Zotero data with book status and update google sheet
   book_update <-
     dplyr::full_join(zotero_data, book_status) %>%
     dplyr::mutate_all(~ifelse(is.na(.), "", .))
-  
+
   # Update googlesheet with new data
   googlesheets::gs_title(sheet_name) %>%
     googlesheets::gs_edit_cells(input = book_update)
-  
+
 }
